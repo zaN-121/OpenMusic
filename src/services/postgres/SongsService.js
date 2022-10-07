99a118723a1665a618276e42721b901f5e5457ab
zaN-121 <projectdsanthos@gmail.com>
2022-10-07 09:21:17 +0700

    Write comment in this file

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -1,13 +1,28 @@
-/* eslint-disable no-underscore-dangle */
+// dependencies
 const { Pool } = require('pg');
 const { nanoid } = require('nanoid');
+
+// errors
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
-const mapDBToModel = require('../../utils');
+
+// utils
+const { mapSongDBToModel } = require('../../utils');
 
 class SongsService {
   constructor() {
     this._pool = new Pool();
+  }
+
+  async verifyIsSongExist(id) {
+    const query = {
+      text: 'SELECT * FROM songs WHERE id = $1',
+      values: [id],
+    };
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu tidak ditemukan');
+    }
   }
 
   async addSong({
@@ -56,7 +71,22 @@ class SongsService {
     if (!result.rows.length) {
       throw new NotFoundError('Lagu tidak ditemukan');
     }
-    return result.rows.map(mapDBToModel)[0];
+    return result.rows.map(mapSongDBToModel)[0];
+  }
+
+  async getSongsInPlaylistByPlaylistId(playlistId) {
+    const query = {
+      text: `SELECT songs.id, songs.title, songs.performer
+      FROM songs LEFT JOIN playlist_songs ON playlist_songs.song_id = songs.id
+      WHERE playlist_songs.playlist_id = $1`,
+      values: [playlistId],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu tidak ditemukan');
+    }
+    return result.rows;
   }
 
   async editSongById(id, {
