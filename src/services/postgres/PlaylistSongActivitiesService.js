222c8f9657ce23362c77225ac365f8b912340a51
zaN-121 <projectdsanthos@gmail.com>
2022-10-07 08:58:43 +0700

    Create playlist song activities service

---
+++ src/services/postgres/PlaylistSongActivitiesService.js
@@ -0,0 +1,35 @@
+// Dependencies
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+
+// Error
+const InvariantError = require('../../exceptions/InvariantError');
+
+class PlaylistSongActivitiesService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addPlaylistSongActivitie(
+    playlist_id,
+    song_id,
+    user_id,
+    action,
+  ) {
+    const id = `pl-sg-act-${nanoid(16)}`;
+    const time = new Date().toISOString();
+
+    const query = {
+      text: `INSERT INTO playlist_song_activities
+      VALUES($1, $2, $3, $4, $5, $6) RETURNING id`,
+      values: [id, playlist_id, song_id, user_id, action, time],
+    };
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new InvariantError('Aktivitas gagal ditambahkan');
+    }
+    return result.rows[0];
+  }
+}
+
+module.exports = PlaylistSongActivitiesService;
