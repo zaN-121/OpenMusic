c1224550e6700d180d2bd48b4c36dc0c59b091e9
zaN-121 <projectdsanthos@gmail.com>
2022-10-07 09:05:14 +0700

    Create authentications api plugin

---
+++ src/api/authentications/handler.js
@@ -0,0 +1,74 @@
+// dependencies
+const autoBind = require('auto-bind');
+
+class AuthenticationsHandler {
+  constructor(
+    authenticationsService,
+    usersService,
+    tokenManager,
+    validator,
+  ) {
+    this._authenticationsService = authenticationsService;
+    this._usersService = usersService;
+    this._tokenManager = tokenManager;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postAuthenticationHandler(request, h) {
+    // validate post payload
+    this._validator.validatePostAuthenticationPayload(request.payload);
+    // verify user credential
+    const { username, password } = request.payload;
+    const id = await this._usersService.verifyUserCredentials(username, password);
+    // generate access & refresh token
+    const accessToken = this._tokenManager.generateAccessToken({ id });
+    const refreshToken = this._tokenManager.generateRefreshToken({ id });
+    await this._authenticationsService.addRefreshToken(refreshToken);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        accessToken,
+        refreshToken,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async putAuthenticationHandler(request) {
+    // validate put payload
+    this._validator.validatePutAuthenticationPayload(request.payload);
+    // verify refresh token
+    const { refreshToken } = request.payload;
+    await this._authenticationsService.verifyRefreshToken(refreshToken);
+    const { id } = this._tokenManager.verifyRefreshToken(refreshToken);
+    // generate accessToken token
+    const accessToken = this._tokenManager.generateAccessToken({ id });
+
+    return {
+      status: 'success',
+      data: {
+        accessToken,
+      },
+    };
+  }
+
+  async deleteAuthenticationHandler(request) {
+    // validate delete payload
+    this._validator.validateDeleteAuthenticationPayload(request.payload);
+    // verify refresh token
+    const { refreshToken } = request.payload;
+    this._tokenManager.verifyRefreshToken(refreshToken);
+    // delete refresh token
+    await this._authenticationsService.deleteRefreshToken(refreshToken);
+
+    return {
+      status: 'success',
+      message: 'Refresh token berhasil dihapus',
+    };
+  }
+}
+module.exports = AuthenticationsHandler;
