39164d5ba9096ec7bb18c76819668efb57bf4c7c
zaN-121 <projectdsanthos@gmail.com>
2022-10-07 09:04:36 +0700

    Create collaborations api plugin

---
+++ src/api/collaborations/handler.js
@@ -0,0 +1,63 @@
+// dependencies
+const autoBind = require('auto-bind');
+
+class CollaborationsHandler {
+  constructor(
+    collaborationsService,
+    playlistsService,
+    usersService,
+    validator,
+  ) {
+    this._collaborationsService = collaborationsService;
+    this._playlistsService = playlistsService;
+    this._usersService = usersService;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postCollaborationHandler(request, h) {
+    await this._validator.validateCollaborationPayload(request.payload);
+    const { playlistId, userId } = request.payload;
+    const { id: credentialId } = request.auth.credentials;
+    // is playlist exist?
+    await this._playlistsService.verifyIsPlaylistExist(playlistId);
+    // is request owner playlist owner?
+    await this._playlistsService.verifyPlaylistOwner(playlistId, credentialId);
+    // is user exixt?
+    await this._usersService.verifyIsUserExist(userId);
+    // is collaborations is exist?
+    await this._collaborationsService.verifyIsCollaborationExist(playlistId, userId);
+
+    const result = await this._collaborationsService.addCollaboration(playlistId, userId);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        collaborationId: result,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async deleteCollaborationByIdHandler(request) {
+    await this._validator.validateCollaborationPayload(request.payload);
+    const { playlistId, userId } = request.payload;
+    const { id: credentialId } = request.auth.credentials;
+    // is playlist exist?
+    await this._playlistsService.verifyIsPlaylistExist(playlistId);
+    // is playlist owner?
+    await this._playlistsService.verifyPlaylistOwner(playlistId, credentialId);
+    // is collaborations exist?
+
+    await this._collaborationsService.deleteCollaboration(playlistId, userId);
+
+    return {
+      status: 'success',
+      message: 'Collaboration berhasil dihapus',
+    };
+  }
+}
+
+module.exports = CollaborationsHandler;
---
+++ src/api/collaborations/index.js
@@ -0,0 +1,24 @@
+const CollaborationsHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+  name: 'collaborations-api',
+  version: '1.0.0',
+  register: async (
+    server,
+    {
+      collaborationsService,
+      playlistsService,
+      usersService,
+      validator,
+    },
+  ) => {
+    const collaborationsHandler = new CollaborationsHandler(
+      collaborationsService,
+      playlistsService,
+      usersService,
+      validator,
+    );
+    server.route(routes(collaborationsHandler));
+  },
+};
